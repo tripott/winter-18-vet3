0c99e99492ba6ee4016d1842cc8f0aca16ed0ed5
tripott <tripott@gmail.com>
2018-03-15 09:34:19 -0400

    adding categories list to dal

--- api/dal.js
+++ api/dal.js
@@ -2,8 +2,11 @@ const { allDocs } = require('./lib/dal-helper')
 
 const getResources = options => allDocs(options || { include_docs: true })
 
+const getCategories = options => allDocs(options || { include_docs: true })
+
 const dal = {
-  getResources
+  getResources,
+  getCategories
 }
 
 module.exports = dal
